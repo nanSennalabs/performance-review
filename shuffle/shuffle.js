373192b2f88f59a0edaaf3d123156019277bf0cd
Nan Chonthicha <nan@sennalabs.com>
2021-01-04 17:34:24 +0700

    shuffle

--- shuffle/shuffle.js
+++ shuffle/shuffle.js
@@ -4,6 +4,11 @@
  * @return {number[]}
  */
 export function shuffle(cards) {
-  throw new Error('Not implemented')
+  // throw new Error('Not implemented')
+  cards.forEach((number, index) => {
+    let shuff = Math.floor(Math.random() * (index + 1));
+    let temp = number[index];
+    number[index] = number[shuff];
+    number[shuff] = temp;
+  });
 }
-
