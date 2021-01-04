87f840d115d8b31379334cf421fb8934458874e4
Nan Chonthicha <nan@sennalabs.com>
2021-01-04 17:15:55 +0700

    loop number mod index

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -1,7 +1,15 @@
 export function isPrime(num) {
   const total = Math.sqrt(num);
-  
+  let cnt;
 
-
-  throw new Error('Not implemented')
+  for (let index = 1; index <= total; i++) {
+    if (total % index === 0) {
+      cnt++;
+    }
+  }
+  if (cnt === 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
