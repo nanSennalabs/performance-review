97608363be36b640c0b7dc406b47d683102fad6a
Nan Chonthicha <nan@sennalabs.com>
2021-01-04 19:03:41 +0700

    find 2 function and attitude

--- mbti/findCognitiveFunction.js
+++ mbti/findCognitiveFunction.js
@@ -10,4 +10,25 @@
 export function findCognitiveFunction(mbti) {
   throw new Error("Not implemented");
   const letters = Array.from(mbti);
+  const attitude = letters[3];
+
+  const dominant = getDominant(letters[1], attitude);
+  const auxiliary = getAuxiliary(letters[2], attitude);
+function getDominant(letter, Att) {
+  switch (Att) {
+    case "J":
+      return letter + "i";
+    default:
+      return letter + "e";
+  }
+}
+
+function getAuxiliary(letter, Att) {
+  switch (Att) {
+    case "J":
+      return letter + "e";
+    default:
+      return letter + "i";
+  }
+}
 }
